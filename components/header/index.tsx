67ecb36619f021667e28e61ece619033151faf62
Nicolás Casmuz <nicolascasmuz@gmail.com>
2025-11-10 23:08:53 -0300

    commit-003

--- components/header/index.tsx
+++ components/header/index.tsx
@@ -5,34 +5,57 @@ import menuPic from "../../resources/menu.svg";
 
 export default function Header() {
   return (
-    <div className={styles["header"]}>
-      <a href="/">
+    <header className={styles["header"]}>
+      <a href="/" className={styles["header__logo-a"]}>
         <Image
           src={logoBuoni}
           alt="logo-header"
-          className={styles["logo-header"]}
+          className={styles["header__logo"]}
           width={50}
           height={50}
         />
       </a>
-      <Image
-        src={menuPic}
-        alt="menu"
-        className={styles.menu}
-        width={50}
-        height={50}
+      <input
+        className={styles["header__menu-input"]}
+        type="checkbox"
+        id="check"
       />
-      <nav className={styles["options-wrapper"]}>
-        <a className={styles["options-header"]} href="/about-us">
+      <label htmlFor="check" className={styles["header__menu-label"]}>
+        <Image
+          src={menuPic}
+          alt="menu"
+          className={styles["header__menu"]}
+          width={50}
+          height={50}
+        />
+      </label>
+      <ul className={styles["header__menu-li"]}>
+        <li>
+          <a href="/" className={styles["header__option"]}>
+            Inicio
+          </a>
+          <a href="/about-us" className={styles["header__option"]}>
+            Quienes somos
+          </a>
+          <a href="/products" className={styles["header__option"]}>
+            Nuestros productos
+          </a>
+          <a href="/contact" className={styles["header__option"]}>
+            Contacto
+          </a>
+        </li>
+      </ul>
+      <nav className={styles["header__menu-nav"]}>
+        <a className={styles["header__option"]} href="/about-us">
           Quienes somos
         </a>
-        <a className={styles["options-header"]} href="/products">
+        <a className={styles["header__option"]} href="/products">
           Nuestros productos
         </a>
-        <a className={styles["options-header"]} href="/contact">
+        <a className={styles["header__option"]} href="/contact">
           Contacto
         </a>
       </nav>
-    </div>
+    </header>
   );
 }
